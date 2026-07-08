007e32cfaca7e02c23b8c9c16f90a70756c3701d
Ankit Dandotiya <dandotiyaankit07@gmail.com>
2026-07-08 19:43:40 +0530

    Prepare Backend for render deployement

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,8 @@ app.use((err, req, res, next) => {
   });
 });
 
+const PORT = process.env.PORT || 5000;
 // SERVER 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
