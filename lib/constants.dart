156adeb160dc74f946e461a0c24bf12ae8972b8c
Ankit Dandotiya <dandotiyaankit07@gmail.com>
2026-07-08 21:12:26 +0530

    Flutter connected to Render Backend

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,11 @@
 class AppConstants {
   // Update this single IP address when your local host machine's IP changes
-  static const String ipAddress = '192.168.1.5';
-  static const String port = '5000';
+   
   
   // Base URLs
-  static const String backendUrl = 'http://$ipAddress:$port';
+  static const String backendUrl =
+      'https://stylencut-backend.onrender.com';
+
   static const String authBaseUrl = '$backendUrl/api/auth';
   static const String salonBaseUrl = '$backendUrl/api/salons';
 }
